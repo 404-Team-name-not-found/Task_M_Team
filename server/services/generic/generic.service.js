2967e0cf31205300f809333ddb98d516bebf6684
alon manshari <alonman17@gmail.com>
2022-05-02 22:10:35 +0300

    Fixed generic service and controller. added message comp

--- server/services/generic/generic.service.js
+++ server/services/generic/generic.service.js
@@ -1,105 +1,103 @@
-const { StatusCodes } = require('http-status-codes');
-const genericQueries = require("../genericCrudQueries")
+const { StatusCodes } = require("http-status-codes");
+const genericQueries = require("../genericCrudQueries");
 
-
-const genericService = (TABLE_NAME)=>{
-    /**
- * Used to get all existing items.
- * 
- * @returns relevant status code and array of all the item objects.
+/**
+ * Used to generate a service for the given item
+ * @param {*} TABLE_NAME The DB table name that co-respone to the item
+ * @returns
  */
-async function getitems() {
+const genericService = (TABLE_NAME) => {
+  /**
+   * Used to get all existing items.
+   *
+   * @returns relevant status code and array of all the item objects.
+   */
+  async function getitems() {
     try {
-       
-        const res = await genericQueries.getItems(TABLE_NAME);
-        return { items: res, status: StatusCodes.OK };
+      const res = await genericQueries.getItems(TABLE_NAME);
+      return { items: res, status: StatusCodes.OK };
     } catch (err) {
-        return { status: StatusCodes.BAD_REQUEST, error: err.message };
+      return { status: StatusCodes.BAD_REQUEST, error: err.message };
     }
-}
+  }
 
-/**
- * Used to get a item by id.
- *
- * @param {number} id (number representing the item's id)
- * 
- * @returns relevant status code and the wanted item object.
- */
-async function getitem(id) {
+  /**
+   * Used to get a item by id.
+   *
+   * @param {number} id (number representing the item's id)
+   *
+   * @returns relevant status code and the wanted item object.
+   */
+  async function getitem(id) {
     try {
-        const isExist = await genericQueries.isExist(TABLE_NAME, "id", id);
-        if (!isExist) throw new Error(`item with this id does not exist`);
+      const isExist = await genericQueries.isExist(TABLE_NAME, "id", id);
+      if (!isExist) throw new Error(`item with this id does not exist`);
 
-        const res = await genericQueries.getItem(TABLE_NAME, "id", id);
-        console.log(res);
-        return { item: res, status: StatusCodes.OK };
+      const res = await genericQueries.getItem(TABLE_NAME, "id", id);
+      console.log(res);
+      return { item: res, status: StatusCodes.OK };
+    } catch (err) {
+      return { status: StatusCodes.BAD_REQUEST, error: err.message };
     }
-    catch (err) {
-        return { status: StatusCodes.BAD_REQUEST, error: err.message };
-    }
-}
+  }
 
-/**
- * Used to add a item.
- *
- * @param {object} newitem (object representing the new item object to add)}
- * 
- * @returns relevant status code.
- */
-async function additem(newitem) {
+  /**
+   * Used to add a item.
+   *
+   * @param {object} newitem (object representing the new item object to add)}
+   *
+   * @returns relevant status code.
+   */
+  async function additem(newitem) {
     try {
-        const isExist = await genericQueries.isExist(TABLE_NAME, "name", newitem.name);
-        if (isExist) throw new Error(`item with this name already exists`);
+      const isExist = await genericQueries.isExist(TABLE_NAME, "name", newitem.name);
+      if (isExist) throw new Error(`item with this name already exists`);
 
-        await genericQueries.insertItem(TABLE_NAME, newitem);
-        return { status: StatusCodes.OK };
+      await genericQueries.insertItem(TABLE_NAME, newitem);
+      return { status: StatusCodes.OK };
+    } catch (err) {
+      return { status: StatusCodes.BAD_REQUEST, error: err.message };
     }
-    catch (err) {
-        return { status: StatusCodes.BAD_REQUEST, error: err.message };
-    }
-}
+  }
 
-/**
- * Used to update a item.
- *
- * @param {object} id (string representing the item's id)
- * @param {object} change (object representing the properties to change)
- *
- * @returns relevant status code.
- */
-async function updateitem(id, change) {
+  /**
+   * Used to update a item.
+   *
+   * @param {object} id (string representing the item's id)
+   * @param {object} change (object representing the properties to change)
+   *
+   * @returns relevant status code.
+   */
+  async function updateitem(id, change) {
     try {
-        const isExist = await genericQueries.isExist(TABLE_NAME, "id", id);
-        if (!isExist) throw new Error(`item with the id- ${id} does not exist`);
-        await genericQueries.updateSpecificItem("id", id, TABLE_NAME, change);
-        return { status: StatusCodes.OK };
+      const isExist = await genericQueries.isExist(TABLE_NAME, "id", id);
+      if (!isExist) throw new Error(`item with the id- ${id} does not exist`);
+      await genericQueries.updateSpecificItem("id", id, TABLE_NAME, change);
+      return { status: StatusCodes.OK };
+    } catch (error) {
+      return { status: StatusCodes.BAD_REQUEST, error: error.message };
     }
-    catch (error) {
-        return { status: StatusCodes.BAD_REQUEST, error: error.message };
-    }
-}
+  }
 
-/**
- * Used to delete a item.
- *
- * @param {number} id (number representing the item's id)
- * 
- * @returns relevant status code.
- */
-async function deleteitem(id) {
+  /**
+   * Used to delete a item.
+   *
+   * @param {number} id (number representing the item's id)
+   *
+   * @returns relevant status code.
+   */
+  async function deleteitem(id) {
     try {
-        const isExist = await genericQueries.isExist(TABLE_NAME, "id", id);
-        if (!isExist) throw new Error(`item with the id- ${id} does not exist`);
-        await genericQueries.deleteItem(TABLE_NAME, "id", id);
-        return { status: StatusCodes.OK };
+      const isExist = await genericQueries.isExist(TABLE_NAME, "id", id);
+      if (!isExist) throw new Error(`item with the id- ${id} does not exist`);
+      await genericQueries.deleteItem(TABLE_NAME, "id", id);
+      return { status: StatusCodes.OK };
+    } catch (error) {
+      return { status: StatusCodes.BAD_REQUEST, error: error.message };
     }
-    catch (error) {
-        return { status: StatusCodes.BAD_REQUEST, error: error.message };
-    }
-}
-   
-    return {getitem,getitems,deleteitem,updateitem,additem};
-}
+  }
 
+  return { getitem, getitems, deleteitem, updateitem, additem };
+};
 
-module.exports =  {genericService} ;
+module.exports = { genericService };
--- server/src/routes/api/message/index.js
+++ server/src/routes/api/message/index.js
@@ -1,48 +1,35 @@
-
-const genericController=require('../../../../services/generic/generic.controller');
-const {genericService}=require('../../../../services/generic/generic.service');
+const genericController = require("../../../../services/generic/generic.controller");
+const { genericService } = require("../../../../services/generic/generic.service");
 
 /**
- * Used as a router for the group's routes.
+ * Used as a router for the message's routes.
  *
  * @param fastify fastify instance
  * @param options
  * @param done
  */
 function messageRoutes(fastify, options, done) {
+  const schema = {
+    id: { type: "number" },
+    title: { type: "string" },
+    author: { type: "string" },
+    content: { type: "string" },
+    taskid: { type: "number" },
+  };
+  const TABLE_NAME = "messages";
 
-    const schema={
-    id: { type: 'number' },
-    name: { type: 'string' },
-    description: { type: 'string' },
-    estimation: { type: 'number' },
-    creationDate: { type: 'string', "format": "date"},
-    endDate: { type: 'string', "format": "date"},
-    status: { type: 'string' },
-    assignedTo: { type: 'number' },
-    creator: { type: 'number' },
-    sprintId: { type: 'number' },
-    storyId: { type: 'number' },
-    groupId: { type: 'number' },
-    };
-    const TABLE_NAME="tasks";
-    
-    
-const messageController=genericController(genericService(TABLE_NAME),schema,'tasks testing');
-    fastify.get('/all', messageController.getItems);
-    
-     fastify.get('/:id', messageController.getItem);
-    
-     fastify.post('/add', messageController.addItem);
-    
-     fastify.patch('/edit/:id', messageController.updateItem);
-    
-     fastify.delete('/:id', messageController.deleteItem);
-    
-    done();
-};
-    
+  const messageController = genericController(genericService(TABLE_NAME), schema, [], "message");
+  fastify.get("/all", messageController.getItems);
+
+  fastify.get("/:id", messageController.getItem);
+
+  fastify.post("/add", messageController.addItem);
+
+  fastify.patch("/edit/:id", messageController.updateItem);
+
+  fastify.delete("/:id", messageController.deleteItem);
+
+  done();
+}
+
 module.exports = messageRoutes;
-
-
-

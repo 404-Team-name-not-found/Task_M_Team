792ba4ac5fe1956df3cb87cbe7e8cd2d643d055c
unknown <Amit1paz@gmail.com>
2022-05-30 21:04:52 +0300

    Login form structure

---
+++ client/src/components/Form/Form.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { StyledForm } from './Form.styled'
+export default function Form() {
+  return (
+    <StyledForm>
+        Form
+    </StyledForm>
+  )
+}
---
+++ client/src/components/Form/Form.styled.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledForm = styled.form`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 25rem; 
+    height: 30rem;
+    padding: 1rem;
+    border-radius: 0.3rem;
+    background-color: #121231;
+    color: white;
+`;

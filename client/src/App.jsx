19e74644949a2a6aab09ada2f0ffc1e66fe606e8
unknown <Amit1paz@gmail.com>
2022-05-31 20:45:13 +0300

    (Feat) ReCAPTCHA v2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Kanban from './components/Kanban/Kanban';
 import KanbanPage from './Pages/KanbanPage';
+import LoginPage from './Pages/LoginPage';
 // import Navbar from './components/Navbar'
 // import SpirntPage from './Pages/SpirntPage'
 // import BacklogPage from './Pages/BacklogPage'
@@ -11,23 +12,25 @@ import KanbanPage from './Pages/KanbanPage';
 
 function App() {
     return (
-        <KanbanPage></KanbanPage>
+        // <KanbanPage></KanbanPage>
         // <Kan
-        // <Router>
-        //     <div className='container'>
-        //         <div className='Navbar'>
-        //             <Navbar />
-        //         </div>
-        //         <div className='apps'>
-        //             <Routes>
-        //                 <Route path="/backlog" element={<BacklogPage />} />
-        //                 <Route path="/sprint" element={<SpirntPage />} />
-        //                 <Route path="/statistics" element={<StatisticsPage />} />
-        //                 <Route path="/dnd" element={<Kanban />} />
-        //             </Routes>
-        //         </div>
-        //     </div>
-        // </Router>
+        <Router>
+            <div className='container'>
+                {/* <div className='Navbar'>
+                    <Navbar />
+                </div> */}
+                <div className='apps'>
+                    <Routes>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/kanban" element={<KanbanPage />} />
+                        {/* <Route path="/backlog" element={<BacklogPage />} />
+                        <Route path="/sprint" element={<SpirntPage />} />
+                        <Route path="/statistics" element={<StatisticsPage />} />
+                        <Route path="/dnd" element={<Kanban />} /> */}
+                    </Routes>
+                </div>
+            </div>
+        </Router>
     )
 }
 
--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { StyledForm, StyledInput, StyledLabel } from './Form.styled';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 export default function Form() {
-  return (
-    <StyledForm action='/'>
-        <h2>Login</h2>
-        <StyledLabel htmlFor="username">Username:</StyledLabel>
-        <StyledInput type="text" id="username" placeholder="Enter username..." required />
-        <StyledLabel htmlFor="password">Username:</StyledLabel>
-        <StyledInput type="password" id="password" placeholder="Enter password..." required />
-    </StyledForm>
-  )
+    const handleCaptchaCahnge = (e) => {
+        console.log(e)
+    }
+    return (
+        <StyledForm action='/'>
+            <h2>Login</h2>
+            <StyledLabel htmlFor="username">Username:</StyledLabel>
+            <StyledInput type="text" id="username" placeholder="Enter username..." required />
+            <StyledLabel htmlFor="password">Password:</StyledLabel>
+            <StyledInput type="password" id="password" placeholder="Enter password..." required />
+            <ReCAPTCHA 
+                sitekey={process.env.RECAPTCHA_V2_KEY_FRONT}
+                onChange={handleCaptchaCahnge}
+            />
+        </StyledForm>
+    )
 }

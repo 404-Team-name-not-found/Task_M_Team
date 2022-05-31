d8f5fc0f74cced733d3f5e068d7e48e42df2f1ac
unknown <Amit1paz@gmail.com>
2022-05-31 19:03:33 +0300

    login form inputs

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import { StyledForm } from './Form.styled'
+import React from 'react';
+import { StyledForm, StyledInput, StyledLabel } from './Form.styled';
+
 export default function Form() {
   return (
-    <StyledForm>
-        Form
+    <StyledForm action='/'>
+        <h2>Login</h2>
+        <StyledLabel htmlFor="username">Username:</StyledLabel>
+        <StyledInput type="text" id="username" placeholder="Enter username..." required />
+        <StyledLabel htmlFor="password">Username:</StyledLabel>
+        <StyledInput type="password" id="password" placeholder="Enter password..." required />
     </StyledForm>
   )
 }
--- client/src/components/Form/Form.styled.jsx
+++ client/src/components/Form/Form.styled.jsx
@@ -1,13 +1,32 @@
 import styled from "styled-components";
 
 export const StyledForm = styled.form`
-    display: flex;
-    justify-content: center;
-    align-items: center;
     width: 25rem; 
     height: 30rem;
-    padding: 1rem;
+    padding: 4rem 2rem;
     border-radius: 0.3rem;
     background-color: #121231;
     color: white;
+
+    h2 {
+        width: 100%;
+        text-align: center;
+        margin-bottom: 3rem;
+    }
 `;
+
+export const StyledLabel = styled.label`
+    font-weight: 600;
+    display: block;
+    margin: 0.5rem 0;
+` ;
+
+export const StyledInput = styled.input`
+    width: 100%;
+    padding: 0.5rem 1rem;
+    background-color: #21213E;
+    border: none;
+    color: white;
+    border-radius: 0.3rem;
+    margin-bottom: 1rem;
+`

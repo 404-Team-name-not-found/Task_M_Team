478cae9d41e995b847f91fd467657121f5540d21
SorerBOT <alonfiller@gmail.com>
2022-06-11 14:06:12 +0300

    (Patch) Handle Input Errors as a PriorityQueue

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -4,7 +4,8 @@ import { usernameFilter } from '../../Regex';
 import formValidation from './FormValidation';
 import ReCAPTCHA from 'react-google-recaptcha';
 import Button from '../Button/Button';
-import formErrors from './formErrors';
+import { setErrorsQueue, defaultErrors } from './formErrors';
+
 export default function Form({ variant }) {
     const RECAPTCHA_KEY = process.env.REACT_APP_RECAPTCHA;
     const recaptchaRef = useRef();
@@ -13,22 +14,21 @@ export default function Form({ variant }) {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState(
-        {
-            status: false,
-            username: formErrors.required,
-            password: formErrors.required,
-            email: formErrors.required,
-            captcha: false
-        }
-    );
-    useEffect(() => {
-        setErrors(formValidation(username, email, password, isChecked));
-        setSubmit(errors.status);
-    }, [username, email, password, isChecked]);
-    const handleCaptchaChange = () => { setIsChecked(recaptchaRef.current.getValue()); }
+    const [repeatPassword, setReapeatPassword] = useState("");
+    const [errors, setErrors] = useState(defaultErrors);
+    const [errorsDisplay, setErrorsDisplay] = useState();
+
+    const handleCaptchaChange = (state) => { setIsChecked(state); }
     const handleUsername = event => { setUsername(event.target.value.replace(usernameFilter, "").slice(0, 16)); }
     const handleEmail = event => { setEmail(event.target.value); }
+    const handlePassword = event => { setPassword(event.target.value); }
+    const handleRepeatPassword = event => { setReapeatPassword(event.target.value); }
+
+    useEffect(() => {
+        setErrors(setErrorsQueue(formValidation(username, email, password, repeatPassword, isChecked, errors)));
+        setSubmit(errors.status.data);
+    }, [username, email, password, repeatPassword, isChecked]);
+
 
     if (variant === "login") return (
         <StyledForm action='/'>
@@ -42,8 +42,8 @@ export default function Form({ variant }) {
                     ref={recaptchaRef}
                     sitekey={RECAPTCHA_KEY}
                     onChange={handleCaptchaCahnge}
-                />
-            </StyledRecaptchaContainer> */}
+                    />
+                </StyledRecaptchaContainer> */}
             <Button
                 disabled={!isChecked}
                 width={12}
@@ -58,20 +58,23 @@ export default function Form({ variant }) {
             <h2>Sign Up</h2>
             <StyledLabel htmlFor="username">Username:</StyledLabel>
             <StyledInput type="text" id="username" value={username} onChange={handleUsername} placeholder="Enter username..." required />
-            <StyledError>{errors.username}</StyledError>
+            <StyledError>{errors.username.getFirst()}</StyledError>
             <StyledLabel htmlFor="email">Email:</StyledLabel>
             <StyledInput type="email" id="email" onChange={handleEmail} placeholder="Enter Email..." required />
-            <StyledError>{errors.email}</StyledError>
+            <StyledError>{errors.email.getFirst()}</StyledError>
             <StyledLabel htmlFor="password">Password:</StyledLabel>
-            <StyledInput type="password" id="password" placeholder="Enter password..." required />
-            <StyledError>{errors.password}</StyledError>
+            <StyledInput type="password" id="password" onChange={handlePassword} placeholder="Enter password..." required />
+            <StyledError>{errors.password.getFirst()}</StyledError>
             <StyledLabel htmlFor="password">Repeat Password:</StyledLabel>
-            <StyledInput type="password" id="password" placeholder="Enter password..." required />
+            <StyledInput type="password" id="password" onChange={handleRepeatPassword} placeholder="Enter password..." required />
+            <StyledError>{errors.repeatPassword.getFirst()}</StyledError>
             <StyledRecaptchaContainer>
                 <ReCAPTCHA
                     ref={recaptchaRef}
                     sitekey={RECAPTCHA_KEY}
-                    onChange={handleCaptchaChange}
+                    onChange={e => { handleCaptchaChange(true); }}
+                    theme={"dark"}
+                    onExpired={e => { handleCaptchaChange(false); }}
                 />
             </StyledRecaptchaContainer>
             <Button
--- client/src/components/Form/FormValidation.jsx
+++ client/src/components/Form/FormValidation.jsx
@@ -4,15 +4,26 @@ import { emailRegex } from "../../Regex";
 const validateEmail = email => {
     return emailRegex.test(email);
 }
-export default function formValidation(username, email, password, captcha) {
-    const data_username = username ? formErrors.none : formErrors.required;
-    const data_email = email ? (validateEmail(email) ? formErrors.none : formErrors.email) : formErrors.required;
-    const data_password = password ? formErrors.none : formErrors.required;
-    const data_status = ([data_username, data_email, data_password].toString().length === 0 && captcha) ? true : false;
+export default function formValidation(username, email, password, repeatPassword, captcha, errors) {
+    const data_username = !!!username;
+    const data_email_required = !!!email;
+    const data_email_invalid = !validateEmail(email);
+    const data_password = !!!password;
+    const data_repeatPassword_required = !!!repeatPassword;
+    const data_repeatPassword_match = !(repeatPassword == password);
+    const data_status = ([data_username, data_email_required, data_email_invalid, data_password, data_repeatPassword_required, data_repeatPassword_match].filter((element) => element).length === 0 && captcha);
+
     return {
-        status: data_status,
-        username: data_username,
-        email: data_email,
-        password: data_password
+        status: { data: data_status },
+        username: { required: { ...errors.username.required, display: data_username } },
+        email: {
+            required: { ...errors.email.required, display: data_email_required },
+            invalid: { ...errors.email.invalid, display: data_email_invalid },
+        },
+        password: { required: { ...errors.password.required, display: data_password } },
+        repeatPassword: {
+            required: { ...errors.repeatPassword.required, display: data_repeatPassword_required },
+            match: { ...errors.repeatPassword.match, display: data_repeatPassword_match }
+        }
     }
 }

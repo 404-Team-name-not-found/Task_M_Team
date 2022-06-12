4485b5a6d42e3378dc7aa0828bd409ca4629e53a
alon manshari <alonman17@gmail.com>
2022-06-12 16:52:27 +0300

    started migration

--- server/src/utils/sqlConnection.utils.js
+++ server/src/utils/sqlConnection.utils.js
@@ -2,7 +2,7 @@ import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+console.log(process.env.POSTGRES_PASSWORD);
 export const pool = new pg.Pool({
   host: process.env.DATABASE_HOST_NAME,
   port: process.env.POSTGRES_PORT,
